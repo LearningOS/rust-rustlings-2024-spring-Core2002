7a38cf1001a0f1efd403ed8755ce294ab563a122
NekokeCore <core2002@163.com>
2024-04-09 15:10:01 +0800

    🎉 All exercises completed! 🎉

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,6 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -29,7 +28,15 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from_node, to_node, weight) = edge;
+
+        if !self.contains(from_node) || !self.contains(to_node) {
+            self.add_node(from_node);
+            self.add_node(to_node);
+        }
+        self.adjacency_table_mutable().get_mut(from_node).unwrap().push((to_node.to_owned(), weight));
+        self.adjacency_table_mutable().get_mut(to_node).unwrap().push((from_node.to_owned(), weight));
+    
     }
 }
 pub trait Graph {
@@ -37,11 +44,20 @@ pub trait Graph {
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        if self.adjacency_table().contains_key(node) {
+            return false;
+        }
+        self.adjacency_table_mutable().insert(node.to_owned(), Vec::new());
+        true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from_node, to_node, weight) = edge;
+        if !self.contains(from_node) || !self.contains(to_node) {
+            panic!("Cannot add edge. One or both nodes don't exist in the graph.");
+        }
+        self.adjacency_table_mutable().get_mut(from_node).unwrap().push((to_node.to_owned(), weight));
+        self.adjacency_table_mutable().get_mut(to_node).unwrap().push((from_node.to_owned(), weight));
+    
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
